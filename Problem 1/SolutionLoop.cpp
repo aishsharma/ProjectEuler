09089e5729816e7f48b179ddeb46e1cd76280ded
aishsharma <asharma3@albany.edu>
2015-05-24 01:31:22 -0400

    Removed unnecessary condition check.

--- Problem 1/SolutionLoop.cpp
+++ Problem 1/SolutionLoop.cpp
@@ -16,10 +16,7 @@ int main() {
 	int sum = 0;
 
 	for (int i = 1; i < 1000; i++) {
-		if ((i % 3 == 0) && (i % 5 == 0)) {
-			sum += i;
-		}
-		else if ((i % 3 == 0) || (i % 5 == 0)) {
+		if ((i % 3 == 0) || (i % 5 == 0)) {
 			sum += i;
 		}
 	}
