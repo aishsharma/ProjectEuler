7db173c8240ed7dc652a16ee5e00649e14363c41
aishsharma <asharma3@albany.edu>
2015-05-26 21:31:30 -0400

    Solved problem 3

---
+++ Problems/Problem 3/Solution.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+double getLPF(double);
+bool isPrime(double);
+
+int main() {
+
+	double num = 600851475143;
+
+	double lpf = getLPF(num);
+
+	if (lpf != -1) {
+		cout << "The largest prime factor of " << num << " is: " << lpf;
+	}
+	else {
+		cout << "This number has no prime factors.";
+	}
+
+
+	cout << "\nPress any key to exit.";
+
+	cin.get();
+	return 0;
+}
+
+double getLPF(double num) {
+	double lpf = -1;
+
+	double root = floor(sqrt(num));
+
+	for (double i = root; i > 1; i--) {
+		if ((fmod(num, i) == 0)) {
+			if (isPrime(i)) {
+				lpf = i;
+				break;
+			}
+		}
+	}
+
+	return lpf;
+}
+
+bool isPrime(double n) {
+	bool flag = true;
+
+	double limit = floor(sqrt(n));
+
+	for (double i = 2; i <= limit; i++) {
+		if (fmod(n, i) == 0) {
+			flag = false;
+			break;
+		}
+	}
+
+	return flag;
+}
